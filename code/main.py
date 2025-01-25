ca6a277d0d49f098f4cc3aba827a05cde05d9024
Matthew Lowe <matthewjlowe11@gmail.com>
2025-01-25 00:20:43 -0500

    Feat: Display the display_surface and allow users to quit/exit

--- code/main.py
+++ code/main.py
@@ -3,6 +3,18 @@ import pygame
 #general setup
 pygame.init()
 
-# (w, h)
+# Create a display_surface and give it a width and height
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+#Setting running variable to true
+running = True
+
+#event loop
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+# Close the game properly
+pygame.quit()

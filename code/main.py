c1a7dce53a261fa92fd8006835728a929595b4ae
Matthew Lowe <matthewjlowe11@gmail.com>
2025-01-25 00:13:19 -0500

    Feat: Create a display_surface , give it a width and height

--- code/main.py
+++ code/main.py
@@ -1,3 +1,8 @@
 import pygame
 
-print(pygame.ver)
+#general setup
+pygame.init()
+
+# (w, h)
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))

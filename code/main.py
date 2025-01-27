8e8f74a171f342a65d03541f1428bfbdd51498a7
Matthew Lowe <matthewjlowe11@gmail.com>
2025-01-27 10:39:58 -0500

    Feat: Set a new window title, and an icon hover

--- code/main.py
+++ code/main.py
@@ -6,9 +6,12 @@ pygame.init()
 # Create a display_surface and give it a width and height
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
 #Setting running variable to true
 running = True
+
+#Set Window title
+title = "Awesome Game Title"
+pygame.display.set_caption(title, "Enjoy!")
 
 #event loop
 while running:
@@ -17,7 +20,7 @@ while running:
             running = False
 
     #draw the game
-    display_surface.fill('red')
+    display_surface.fill('#333333')
     pygame.display.update()
 
 

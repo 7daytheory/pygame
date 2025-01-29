4ea8f976f75e4a49169c490412dc04c5203c096a
Matthew Lowe <matthewjlowe11@gmail.com>
2025-01-28 21:50:23 -0500

    Fix: Move your ship(the player) on top over all other images on display surface

--- code/main.py
+++ code/main.py
@@ -21,11 +21,13 @@ surf.fill("red")
 x = 100
 
 #importing an image
-ship_path = join('images', 'ship.png')
-ship_surf = pygame.image.load(ship_path).convert_alpha()
 star_path = join('images', 'star.png')
 star_surf = pygame.image.load(star_path).convert_alpha()
 star_pos = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)] # Create tuple to store x and y before the while loop
+
+#Keep the main image(you) on the top of all other images
+ship_path = join('images', 'ship.png')
+ship_surf = pygame.image.load(ship_path).convert_alpha()
 
 #event loop
 while running:

fe14bb2eaf095c1d95d829447a991ed0dd0c1da8
Matthew Lowe <matthewjlowe11@gmail.com>
2025-01-28 22:35:07 -0500

    Feat: Combine join and image load to make code more efficient, remove animation

--- code/main.py
+++ code/main.py
@@ -22,16 +22,13 @@ enemy_x = 50
 x = 100
 
 #importing an image
-star_path = join('images', 'palyer.png')
-star_surf = pygame.image.load(star_path).convert_alpha()
+star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_pos = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)] # Create tuple to store x and y before the while loop
 
-enemy_path = join("images", "enemy.png")
-enemy_surf = pygame.image.load(enemy_path).convert_alpha()
+enemy_surf = pygame.image.load(join("images", "enemy.png")).convert_alpha()
 
 #Keep the main image(you) on the top of all other images
-player_path = join('images', 'ship.png')
-player_surf = pygame.image.load(player_path).convert_alpha()
+player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
 
 #event loop
 while running:
@@ -40,8 +37,6 @@ while running:
             running = False
     #draw the game
     display_surface.fill('#999999')
-    x += 0.1
-    enemy_x += 0.2
     for pos in star_pos:
         display_surface.blit(star_surf, pos)
     display_surface.blit(enemy_surf, (enemy_x, 450))

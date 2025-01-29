a7d43f52d352439bfc7385cf805e1bb040217a43
Matthew Lowe <matthewjlowe11@gmail.com>
2025-01-28 21:56:18 -0500

    Feat: Add an enemy ship going left to right on screen

--- code/main.py
+++ code/main.py
@@ -18,12 +18,16 @@ pygame.display.set_caption(title, "Enjoy!")
 #Surface
 surf = pygame.Surface((100, 200))
 surf.fill("red")
+enemy_x = 50
 x = 100
 
 #importing an image
 star_path = join('images', 'star.png')
 star_surf = pygame.image.load(star_path).convert_alpha()
 star_pos = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)] # Create tuple to store x and y before the while loop
+
+enemy_path = join("images", "enemy.png")
+enemy_surf = pygame.image.load(enemy_path).convert_alpha()
 
 #Keep the main image(you) on the top of all other images
 ship_path = join('images', 'ship.png')
@@ -37,9 +41,11 @@ while running:
     #draw the game
     display_surface.fill('#999999')
     x += 0.1
+    enemy_x += 0.2
     for pos in star_pos:
         display_surface.blit(star_surf, pos)
     display_surface.blit(ship_surf, (x, 150))
+    display_surface.blit(enemy_surf, (enemy_x, 450))
     pygame.display.update()
 
 
